db12650e85bc94a766c63545a9cf4cb2a12099b5
Juan Villalobos <juan.villalobos1992@gmail.com>
2022-04-21 14:28:26 -0700

    some more testing for music NFT contract

--- test/musicMakerNFT.test.js
+++ test/musicMakerNFT.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('MusicMakerNFT', function () {
-  it("Should return the new nft once it's deployed", async function () {
+  it('Should set the platformContractAddress', async function () {
     const fakePlatformAddress = '0x400830aC1BC7956ae4b81C2658d9CC08824FEaEB';
 
     const MusicMakerNFT = await ethers.getContractFactory('MusicMakerNFT');
@@ -44,5 +44,50 @@ describe('MusicMakerNFT', function () {
 
     expect(firstTokenURI).to.equal(fakeLocationOne);
     expect(secondTokenURI).to.equal(fakeLocationTwo);
+  });
+
+  it('Should mint 10 nfts to the same URI', async function () {
+    const fakePlatformAddress = '0x400830aC1BC7956ae4b81C2658d9CC08824FEaEB';
+    const fakeLocationOne = 'https://www.mytokenlocation.com';
+
+    const MusicMakerNFT = await ethers.getContractFactory('MusicMakerNFT');
+    const musicMakerNft = await MusicMakerNFT.deploy(fakePlatformAddress);
+    await musicMakerNft.deployed();
+
+    let publicMintingPrice = await musicMakerNft.PUBLIC_MINTING_PRICE();
+    publicMintingPrice = (Number(publicMintingPrice) * 10).toString();
+
+    await musicMakerNft.mint(fakeLocationOne, 10, {
+      value: publicMintingPrice
+    });
+
+    const contractBalance = await musicMakerNft.getContractBalance();
+    const expectedBalance = ethers.utils.parseUnits('10', 'ether');
+    expect(contractBalance.toString()).to.equal(expectedBalance);
+
+    const firstTokenURI = await musicMakerNft.tokenURI(0);
+    const secondTokenURI = await musicMakerNft.tokenURI(4);
+    const lastTokenURI = await musicMakerNft.tokenURI(9);
+
+    expect(firstTokenURI).to.equal(fakeLocationOne);
+    expect(lastTokenURI).to.equal(fakeLocationOne);
+  });
+
+  it('Should revert if trying to mint > MAX_PUBLIC_MINT', async function () {
+    const fakePlatformAddress = '0x400830aC1BC7956ae4b81C2658d9CC08824FEaEB';
+    const fakeLocationOne = 'https://www.mytokenlocation.com';
+
+    const MusicMakerNFT = await ethers.getContractFactory('MusicMakerNFT');
+    const musicMakerNft = await MusicMakerNFT.deploy(fakePlatformAddress);
+    await musicMakerNft.deployed();
+
+    let publicMintingPrice = await musicMakerNft.PUBLIC_MINTING_PRICE();
+    publicMintingPrice = (Number(publicMintingPrice) * 11).toString();
+
+    await expect(
+      musicMakerNft.mint(fakeLocationOne, 11, {
+        value: publicMintingPrice
+      })
+    ).to.be.revertedWith('Music :: Cannot mine more than 10 at a time!');
   });
 });
